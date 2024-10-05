d94b6bad9eb473b462610a37af982bdbeedd4c50
Burhanuddin Khatri <144617735+BurhanCantCode@users.noreply.github.com>
2024-10-05 14:44:43 -0700

    Updated middleware.ts again

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,29 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { withClerkMiddleware, getAuth } from "@clerk/nextjs/server";
+import type { NextRequest } from "next/server";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/sign-in", "/sign-up"]
+const publicPaths = ["/", "/sign-in*", "/sign-up*"];
+
+const isPublic = (path: string) => {
+  return publicPaths.find(x =>
+    path.match(new RegExp(`^${x}$`.replace("*$", "($|/)")))
+  );
+};
+
+export default withClerkMiddleware((request: NextRequest) => {
+  if (isPublic(request.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+  // if the user is not signed in redirect them to the sign in page.
+  const { userId } = getAuth(request);
+
+  if (!userId) {
+    // redirect the users to /pages/sign-in/[[...index]].ts
+    const signInUrl = new URL('/sign-in', request.url);
+    signInUrl.searchParams.set('redirect_url', request.url);
+    return NextResponse.redirect(signInUrl);
+  }
+  return NextResponse.next();
 });
 
 export const config = {
